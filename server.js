9f9ed31ed2c865031ad22f6a2bedca6a931f3762
Ben Lu <ben@ben-l.com>
2016-07-08 01:16:50 +1200

    updated server to use http

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 var app = require('./app');
 var https = require('https');
+var http = require('http');
 var fs = require('fs')
 var express = require('express');
 
-var options = {
-  key: fs.readFileSync('./key.pem')
-, cert: fs.readFileSync('./cert.pem')
-, requestCert: false
-, rejectUnauthorized: false
-}
 var server = https.createServer(options, app).listen(4000)
+
+try {
+  fs.statSync('./key.pem')
+  fs.statSync('./cert.pem')
+  var options = {
+    key: fs.readFileSync('./key.pem')
+  , cert: fs.readFileSync('./cert.pem')
+  , requestCert: false
+  , rejectUnauthorized: false
+  }
+  https.createServer(options, app).listen(4000)
+} catch(e) {
+  http.createServer(app).listen(4000)
+}
